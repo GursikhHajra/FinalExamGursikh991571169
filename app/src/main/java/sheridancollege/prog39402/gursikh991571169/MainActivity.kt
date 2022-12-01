b2014fb2dbe9fac45f216187780c8a7bee0a973f
gursi <gursikh>
2022-12-01 13:30:27 -0500

    setup done

--- app/src/main/java/sheridancollege/prog39402/gursikh991571169/MainActivity.kt
+++ app/src/main/java/sheridancollege/prog39402/gursikh991571169/MainActivity.kt
@@ -2,10 +2,17 @@ package sheridancollege.prog39402.gursikh991571169
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import sheridancollege.prog39402.gursikh991571169.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        //(activity as AppCompatActivity).supportActionBar?.title = "Home Page"
     }
 }
---
+++ app/src/main/java/sheridancollege/prog39402/gursikh991571169/TitleFragment.kt
@@ -0,0 +1,21 @@
+package sheridancollege.prog39402.gursikh991571169
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import sheridancollege.prog39402.gursikh991571169.databinding.FragmentTitleBinding
+
+class TitleFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+       val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+           inflater, R.layout.fragment_title, container, false)
+
+        return binding.root
+    }
+}
