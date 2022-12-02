bcf537b5af66b1aa3875c4f2e6617e9e490a45c4
gursi <gursikh>
2022-12-02 07:00:07 -0500

    2

--- app/src/main/java/sheridancollege/prog39402/gursikh991571169/TitleFragment.kt
+++ app/src/main/java/sheridancollege/prog39402/gursikh991571169/TitleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import sheridancollege.prog39402.gursikh991571169.databinding.FragmentTitleBinding
 
@@ -15,6 +16,8 @@ class TitleFragment: Fragment() {
         // Inflate the layout for this fragment
        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
            inflater, R.layout.fragment_title, container, false)
+
+        (activity as AppCompatActivity).supportActionBar?.title = "Home Page"
 
         return binding.root
     }
